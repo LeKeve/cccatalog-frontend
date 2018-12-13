c53eb8818132dc60b4ac561225fd382443628c17
Kevin Lee <kevinyjlee23@gmail.com>
2018-12-12 20:57:38 -0800

    [cccatalog-frontend#117] Implement add image to list on image page

--- src/pages/PhotoDetailPage.vue
+++ src/pages/PhotoDetailPage.vue
@@ -4,79 +4,85 @@
       <header-section showNavSearch="true" fixedNav="true"></header-section>
     </div>
     <div class="photo grid-x">
-        <div class="photo_image-ctr cell medium-12 large-8">
-          <a class="photo_breadcrumb"
-             :href="breadCrumbURL"
-             @click.prevent="onGotoBack"
-             v-if="shouldShowBreadcrumb">&#171; Back to search results</a>
-          <img @click="onShowViewer(image)"
-               @load="onLoad"
-               :class="{ photo_image: true,
+      <div class="photo_image-ctr cell medium-12 large-8">
+        <a
+          class="photo_breadcrumb"
+          :href="breadCrumbURL"
+          @click.prevent="onGotoBack"
+          v-if="shouldShowBreadcrumb"
+        >&#171; Back to search results</a>
+        <img
+          @click="onShowViewer(image)"
+          @load="onLoad"
+          :class="{ photo_image: true,
                          'photo_image__has-viewer': this.images.length > 0 }"
-                         :src="image.url">
-        </div>
-        <div class="photo_info-ctr cell medium-12 large-4">
+          :src="image.url"
+        >
+      </div>
+      <div class="photo_info-ctr cell medium-12 large-4">
+        <header class="photo_info-header">
+          <h2>PHOTO INFO</h2>
+        </header>
+        <ul>
+          <li>
+            <h3>Title</h3>
+            <span>{{ image.title }}</span>
+          </li>
+          <li>
+            <h3>Creator</h3>
+            <span>
+              <a :href="image.creator_url">{{ image.creator }}</a>
+            </span>
+          </li>
+          <li>
+            <h3>License</h3>
+            <a
+              class="photo_license"
+              :href="ccLicenseURL"
+            >CC {{ image.license }} {{ image.license_version }}</a>
+            <license-icons :image="image"></license-icons>
+          </li>
+          <li>
+            <h3>Source</h3>
+            <a
+              class="photo_provider"
+              :href="image.foreign_landing_url"
+              target="blank"
+              rel="noopener noreferrer"
+            >{{ image.provider }}</a>
+          </li>
+          <li>
+            <h3>Dimensions</h3>
+            <span>
+              {{ imageWidth }}
+              <span>X</span>
+              {{ imageHeight }} pixels
+            </span>
+          </li>
+        </ul>
+        <section class="photo_usage">
           <header class="photo_info-header">
-            <h2>
-              PHOTO INFO
-            </h2>
+            <h2>Photo Attribution</h2>
           </header>
-          <ul>
-            <li>
-              <h3>Title</h3>
-              <span>{{ image.title }}</span>
-            </li>
-            <li>
-              <h3>Creator</h3>
-              <span>
-              <a :href="image.creator_url">{{ image.creator }}</a>
-              </span>
-            </li>
-            <li>
-              <h3>License</h3>
-              <a class="photo_license" :href="ccLicenseURL">
-              CC {{ image.license }} {{ image.license_version }}
-              </a>
-              <license-icons :image="image"></license-icons>
-            </li>
-            <li>
-              <h3>Source</h3>
-              <a class="photo_provider"
-                 :href="image.foreign_landing_url"
-                 target="blank"
-                 rel="noopener noreferrer">{{ image.provider }}</a>
-            </li>
-            <li>
-              <h3>Dimensions</h3>
-              <span> {{ imageWidth }} <span> X </span> {{ imageHeight }} pixels</span>
-            </li>
-          </ul>
-          <section class="photo_usage">
-            <header class="photo_info-header">
-              <h2>
-                Photo Attribution
-              </h2>
-            </header>
-            <p class="photo_usage-attribution" ref="photoAttribution">
-              <a :href="image.foreign_landing_url">"{{ image.title }}"</a>
-              by
-              <a :href="image.creator_url">{{ image.creator }}</a>
-              is licensed under
-              <a class="photo_license" :href="ccLicenseURL">
-              CC {{ image.license}} {{ image.license_version }}
-              </a>
-            </p>
-            <button class="button photo_copy-btn
-              photo_copy-btn__html"
-              data-type="html">
-            Copy to HTML</button>
-            <button class="button
-              photo_copy-btn
-              photo_copy-btn__text"
-              data-type="text">
-            Copy to Text
-            </button>
-          </section>
+          <p class="photo_usage-attribution" ref="photoAttribution">
+            <a :href="image.foreign_landing_url">"{{ image.title }}"</a>
+            by
+            <a :href="image.creator_url">{{ image.creator }}</a>
+            is licensed under
+            <a
+              class="photo_license"
+              :href="ccLicenseURL"
+            >CC {{ image.license}} {{ image.license_version }}</a>
+          </p>
+          <button class="button photo_copy-btn photo_copy-btn__html" data-type="html">Copy to HTML</button>
+          <button class="button photo_copy-btn photo_copy-btn__text" data-type="text">Copy to Text</button>
+          <button
+            class="button photo_copy-btn"
+            @click.stop="onAddToImageList(image, $event)"
+            v-if="includeAddToList"
+          >Add image to list</button>
+          <share-list></share-list>
+        </section>
       </div>
     </div>
     <div class="photo_tags grid-x full" v-if="tags && tags.length">
@@ -85,21 +91,25 @@
       </header>
       <div class="photo_tags-ctr cell large-12">
         <template v-for="(tag, index) in image.tags" v-if="tag.name">
-          <span class="photo_tag button hollow secondary"
-                :key="index"
-                @click="onGotoSearchPage(tag.name)">
+          <span
+            class="photo_tag button hollow secondary"
+            :key="index"
+            @click="onGotoSearchPage(tag.name)"
+          >
             <span class="photo_tag-label">
               <span>{{ tag.name }}</span>
             </span>
-            <img class="photo_tag-provider-badge"
-                 src="@/assets/clarifai_logo.png"
-                 v-if="isClarifaiTag(tag.provider)">
+            <img
+              class="photo_tag-provider-badge"
+              src="@/assets/clarifai_logo.png"
+              v-if="isClarifaiTag(tag.provider)"
+            >
           </span>
         </template>
         <p class="photo_tags-clarifai-badge" v-if="hasClarifaiTags">
           <span>Tag by</span>
           <a href="https://clarifai.com/">
-            <img class="photo_tags-clarifai-badge-image" src="../assets/clarifai.svg" >
+            <img class="photo_tags-clarifai-badge-image" src="../assets/clarifai.svg">
           </a>
         </p>
       </div>
@@ -116,8 +126,8 @@
         :includeAnalytics="false"
         :useInfiniteScroll="false"
         :includeAddToList="false"
-        v-if="isPrimaryImageLoaded===true">
-      </search-grid>
+        v-if="isPrimaryImageLoaded===true"
+      ></search-grid>
     </div>
     <footer-section></footer-section>
     <viewer :images="images" ref="imageViewer" v-if="isPrimaryImageLoaded===true">
@@ -138,7 +148,8 @@ import Clipboard from 'clipboard';
 import 'viewerjs/dist/viewer.css';
 import Viewer from 'v-viewer';
 import Vue from 'vue';
-import { SET_IMAGE } from '@/store/mutation-types';
+import { SELECT_IMAGE_FOR_LIST, SET_IMAGE } from '@/store/mutation-types';
+import ShareList from '@/components/ShareList';
 
 Vue.use(Viewer);
 
@@ -149,9 +160,13 @@ const PhotoDetailPage = {
     SearchGrid,
     FooterSection,
     LicenseIcons,
+    ShareList,
   },
   props: {
     id: '',
+    includeAddToList: {
+      default: true,
+    },
   },
   data: () => ({
     breadCrumbURL: '',
@@ -191,7 +206,9 @@ const PhotoDetailPage = {
 
       const image = this.image;
       const BASE_URL = 'https://creativecommons.org';
-      let url = `${BASE_URL}/licenses/${image.license}/${image.license_version}`;
+      let url = `${BASE_URL}/licenses/${image.license}/${
+        image.license_version
+      }`;
       let license = '';
 
       if (image.license) {
@@ -209,8 +226,7 @@ const PhotoDetailPage = {
     },
     textAttribution() {
       const image = this.image;
-      const licenseURL =
-        `<a href="${this.ccLicenseURL}">
+      const licenseURL = `<a href="${this.ccLicenseURL}">
           CC ${image.license.toUpperCase()} ${image.license_version}
          </a>`;
 
@@ -247,7 +263,7 @@ const PhotoDetailPage = {
     next();
   },
   beforeRouteEnter(to, previousPage, nextPage) {
-    nextPage((_this) => {
+    nextPage(_this => {
       if (previousPage.name === 'browse-page') {
         _this.shouldShowBreadcrumb = true; // eslint-disable-line no-param-reassign
         _this.breadCrumbURL = `/search?q=${previousPage.query.q}`; // eslint-disable-line no-param-reassign
@@ -265,16 +281,23 @@ const PhotoDetailPage = {
       const tagsParam = (tags || []).slice();
 
       if (tagsParam.length > 0) {
-        queryParam = tagsParam.slice(0, 1).map(tag => tag.name).join(', ');
+        queryParam = tagsParam
+          .slice(0, 1)
+          .map(tag => tag.name)
+          .join(', ');
       }
 
       if (queryParam) {
-        this.$store.dispatch(FETCH_RELATED_IMAGES, { q: queryParam, pagesize: 8 });
+        this.$store.dispatch(FETCH_RELATED_IMAGES, {
+          q: queryParam,
+          pagesize: 8,
+        });
       }
     },
     initClipboard() {
-      new Clipboard('.photo_copy-btn', { // eslint-disable-line no-new
-        text: (element) => {
+      new Clipboard('.photo_copy-btn', {
+        // eslint-disable-line no-new
+        text: element => {
           let attributionContent;
           if (element.getAttribute('data-type') === 'html') {
             attributionContent = this.HTMLAttribution;
@@ -323,6 +346,14 @@ const PhotoDetailPage = {
         viewer.show();
       }
     },
+    onAddToImageList(image, event) {
+      console.log('here');
+      const imageWithDimensions = image || {};
+      imageWithDimensions.pageX = event.pageX;
+      imageWithDimensions.pageY = event.pageY;
+
+      this.$store.commit(SELECT_IMAGE_FOR_LIST, { image: imageWithDimensions });
+    },
   },
   created() {
     this.loadImage(this.$route.params.id);
@@ -335,218 +366,212 @@ export default PhotoDetailPage;
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .photo_image__has-viewer {
-    cursor: zoom-in;
-  }
+.photo_image__has-viewer {
+  cursor: zoom-in;
+}
 
-  .photo_image-viewer {
-    display: none;
-  }
+.photo_image-viewer {
+  display: none;
+}
 
-  .photo-detail-page {
-    width: 100%;
-  }
+.photo-detail-page {
+  width: 100%;
+}
 
-  .photo_license {
-    text-transform: uppercase;
-  }
+.photo_license {
+  text-transform: uppercase;
+}
 
-  .photo_provider {
-     text-transform: capitalize;
-  }
+.photo_provider {
+  text-transform: capitalize;
+}
 
-  .photo_paginator {
-    position: absolute;
-    display: block;
-    width: 50px;
-    height: 50px;
-    z-index: 400;
-    top: 50%;
-    opacity: .5;
-  }
+.photo_paginator {
+  position: absolute;
+  display: block;
+  width: 50px;
+  height: 50px;
+  z-index: 400;
+  top: 50%;
+  opacity: 0.5;
+}
 
-  .photo_paginator__previous {
-    left: 5px;
-    background: url('../assets/arrow-icon_left.svg')
-                center
-                center
-                no-repeat;
-  }
+.photo_paginator__previous {
+  left: 5px;
+  background: url('../assets/arrow-icon_left.svg') center center no-repeat;
+}
 
-  .photo_paginator__next {
-    right: 5px;
-    background: url('../assets/arrow-icon_right.svg')
-                center
-                center
-                no-repeat;
-  }
+.photo_paginator__next {
+  right: 5px;
+  background: url('../assets/arrow-icon_right.svg') center center no-repeat;
+}
 
-  .search-grid {
-    margin: 0;
-    width: 100%;
-  }
+.search-grid {
+  margin: 0;
+  width: 100%;
+}
 
-  .photo {
-    width: 100%;
-    border-bottom: 1px solid #d6d6d6;
-  }
+.photo {
+  width: 100%;
+  border-bottom: 1px solid #d6d6d6;
+}
 
-  .photo_breadcrumb {
-    width: 100%;
-    display: block;
-    text-align: left;
-    font-size:.9em;
-    margin-top: -10px;
-  }
+.photo_breadcrumb {
+  width: 100%;
+  display: block;
+  text-align: left;
+  font-size: 0.9em;
+  margin-top: -10px;
+}
 
-  .photo_image-ctr {
+.photo_image-ctr {
+  position: relative;
+  text-align: center;
+  padding: 30px;
+  max-height: 640px;
+
+  img {
     position: relative;
-    text-align: center;
-    padding: 30px;
-    max-height: 640px;
-
-    img {
-      position: relative;
-      width: auto;
-      height: auto;
-      max-height: 100%;
-      max-width: 100%;
-      background: #EBECE4;
-    }
-
-    /* Small only */
-    @media screen and (max-width: 39.9375em) {
-      padding: 30px 15px;
-    }
+    width: auto;
+    height: auto;
+    max-height: 100%;
+    max-width: 100%;
+    background: #ebece4;
   }
 
-  .photo_info-ctr {
-    padding: 45px 15px;
-    border-left: 1px solid #d6d6d6;
-
-    .photo_info-header {
-      border-top: 1px solid #e7e8e9;
-      margin-left: 15px;
-    }
-
-    h4 {
-      font-size: 1.25rem;
-      font-weight: 500;
-      line-height: 1rem;
-      margin: 0;
-      padding: 0 0 0 15px;
-    }
-
-    h3 {
-      font-size: 1rem;
-      font-weight: 500;
-      line-height: 1rem;
-      margin: 0;
-    }
-
-    h2 {
-      margin-bottom: 1.07142857em;
-      font-size: .875em;
-      font-weight: 600;
-      letter-spacing: 1px;
-      line-height: 1.25;
-      text-transform: uppercase;
-      display: inline-block;
-      padding-top: .28571429em;
-      border-top: 5px solid rgba(29, 31, 39, .8);
-      margin-top: -3px;
-    }
-
-    ul {
-      margin: 0;
-      list-style-type: none;
-
-      li {
-        padding-left: 15px;
-        margin-left: 0;
-        margin-bottom: 10px;
-      }
-    }
-
-    /* Small only */
-    @media screen and (max-width: 39.9375em) {
-      padding: 30px 0;
-    }
+  /* Small only */
+  @media screen and (max-width: 39.9375em) {
+    padding: 30px 15px;
   }
+}
 
-  .photo_usage {
-    .photo_info-header {
-      margin-left: 0px;
-    }
+.photo_info-ctr {
+  padding: 45px 15px;
+  border-left: 1px solid #d6d6d6;
 
-    padding: 15px;
-  }
-
-  .photo_related-images,
-  .photo_tags {
-    margin: 30px;
+  .photo_info-header {
     border-top: 1px solid #e7e8e9;
-    width: 100%;
-
-    header h2 {
-      margin-bottom: 1.07142857em;
-      width: 100%;
-      font-size: 1em;
-      font-weight: 600;
-      letter-spacing: 1px;
-      line-height: 1.25;
-      text-transform: uppercase;
-      display: inline-block;
-      padding-top: .28571429em;
-      border-top: 5px solid rgba(29, 31, 39, 0.8);
-      margin-top: -3px;
-    }
-
-    /* Small only */
-    @media screen and (max-width: 39.9375em) {
-      margin: 15px;
-    }
+    margin-left: 15px;
   }
 
-  .photo_tag {
-    margin-right: 15px;
-    border-radius: 3px;
-    padding: 10px 10px;
-  }
-
-  .photo_tag-label {
+  h4 {
+    font-size: 1.25rem;
     font-weight: 500;
+    line-height: 1rem;
+    margin: 0;
+    padding: 0 0 0 15px;
   }
 
-  .photo_tag-provider-badge {
-    width: 16px;
-    margin-left: 5px;
+  h3 {
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1rem;
+    margin: 0;
   }
 
-  .photo_tags-clarifai-badge {
-    margin-top: 30px;
+  h2 {
+    margin-bottom: 1.07142857em;
+    font-size: 0.875em;
+    font-weight: 600;
+    letter-spacing: 1px;
+    line-height: 1.25;
+    text-transform: uppercase;
+    display: inline-block;
+    padding-top: 0.28571429em;
+    border-top: 5px solid rgba(29, 31, 39, 0.8);
+    margin-top: -3px;
   }
 
-  .photo_tags-clarifai-badge-image {
-    height: 20px;
+  ul {
+    margin: 0;
+    list-style-type: none;
+
+    li {
+      padding-left: 15px;
+      margin-left: 0;
+      margin-bottom: 10px;
+    }
   }
 
-  .photo_usage-attribution {
-    border-left: 1px solid #e7e8e9;;
-    padding-left: 10px;
-    padding-bottom: 15px
+  /* Small only */
+  @media screen and (max-width: 39.9375em) {
+    padding: 30px 0;
+  }
+}
+
+.photo_usage {
+  .photo_info-header {
+    margin-left: 0px;
   }
 
-  .photo_copy-btn {
-    border-radius: 3px;
-    width: 49%;
+  padding: 15px;
+}
+
+.photo_related-images,
+.photo_tags {
+  margin: 30px;
+  border-top: 1px solid #e7e8e9;
+  width: 100%;
+
+  header h2 {
+    margin-bottom: 1.07142857em;
+    width: 100%;
+    font-size: 1em;
+    font-weight: 600;
+    letter-spacing: 1px;
+    line-height: 1.25;
+    text-transform: uppercase;
+    display: inline-block;
+    padding-top: 0.28571429em;
+    border-top: 5px solid rgba(29, 31, 39, 0.8);
+    margin-top: -3px;
   }
 
-  .photo_copy-btn__html {
-    background: #4A69CA;
+  /* Small only */
+  @media screen and (max-width: 39.9375em) {
+    margin: 15px;
   }
+}
 
-  .photo_copy-btn__text {
-    background: #4A69CA;
-  }
+.photo_tag {
+  margin-right: 15px;
+  border-radius: 3px;
+  padding: 10px 10px;
+}
+
+.photo_tag-label {
+  font-weight: 500;
+}
+
+.photo_tag-provider-badge {
+  width: 16px;
+  margin-left: 5px;
+}
+
+.photo_tags-clarifai-badge {
+  margin-top: 30px;
+}
+
+.photo_tags-clarifai-badge-image {
+  height: 20px;
+}
+
+.photo_usage-attribution {
+  border-left: 1px solid #e7e8e9;
+  padding-left: 10px;
+  padding-bottom: 15px;
+}
+
+.photo_copy-btn {
+  border-radius: 3px;
+  width: 49%;
+}
+
+.photo_copy-btn__html {
+  background: #4a69ca;
+}
+
+.photo_copy-btn__text {
+  background: #4a69ca;
+}
 </style>
